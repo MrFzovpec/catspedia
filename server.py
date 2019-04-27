2807af92153fdcfd908c28f6fae882c672dfb8e1
Fzovpec <obgpsuper@gmail.com>
2019-04-27 16:24:46 +0300

    Mongo

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect
 import json
 import datetime
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
-
-file = open('cats.json', 'r')
-text = file.read()
-cats = json.loads(text)
-
-file.close()
-
+client = MongoClient('mongodb://Petr:GPpetr1309@cluster0-shard-00-00-nli2o.mongodb.net:27017,cluster0-shard-00-01-nli2o.mongodb.net:27017,cluster0-shard-00-02-nli2o.mongodb.net:27017/cats?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
+db = client['catspedia']
+collection = db.cats
+cats = collection.find()
 
 @app.route('/', methods=['GET'])
 def index():
+    cats = collection.find()
     return render_template('index.html', cats=cats)
 
 
@@ -35,54 +35,43 @@ def add():
         "comments": [],
         "likes": 0
     }
-    cats.append(cat)
-    file = open('cats.json', 'w')
-    file.write(json.dumps(cats))
-    file.close()
-    return redirect('/cats/{0}'.format(len(cats)))
+    return redirect('/cats/{0}'.format(cats.count()))
 
 
 @app.route('/cats/<id>', methods=['GET'])
 def details(id):
-    file = open('cats.json', 'r')
-    text = file.read()
-    cats = json.loads(text)
-    file.close()
+    cats = collection.find()
     cat = cats[int(id) - 1]
     return render_template('details.html', cat=cat, id=id)
 
 
 @app.route('/like/<id>', methods=['GET'])
 def like(id):
-    file = open('cats.json', 'r')
-    text = file.read()
-    text = json.loads(text)
-    file.close()
-    cat = text[int(id) - 1]
+    cats = collection.find()
+    cat = cats[int(id) - 1]
     cat['likes'] += 1
-    file = open('cats.json', 'w')
-    file.write(json.dumps(text))
-    file.close()
+    id_send = { "_id": cat['_id'] }
+    collection.update_one(id_send, {"$set":cat})
     return redirect('/cats/{0}'.format(int(id)))
 
 
 @app.route('/comment/<id>', methods=['POST'])
 def comment(id):
-    file = open('cats.json', 'r')
-    text = file.read()
-    text = json.loads(text)
-    file.close()
-    cat = text[int(id) - 1]
+    cats = collection.find()
+    cat = cats[int(id) - 1]
     whole_time = datetime.datetime.now()
+    fields = ['author', 'text']
+    for field in fields:
+        if request.form.get(field, '') == '':
+            return redirect('/cats/<id>')
     comment = {
         "author": request.form['author'],
         "text": request.form['text'],
         "date": str(whole_time.date())+"\n"+ str(whole_time.hour) + ":" + str(whole_time.minute),
     }
     cat['comments'].append(comment)
-    file = open('cats.json', 'w')
-    file.write(json.dumps(text))
-    file.close()
+    id_send = { "_id": cat['_id'] }
+    collection.update_one(id_send, {"$set":cat})
     return redirect('/cats/{0}'.format(int(id)))
 
 
